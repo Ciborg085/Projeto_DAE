8e7db78910bb271497d8c9de90dedcc9632f423a
Diagoalv <78082444+Diagoalv@users.noreply.github.com>
2024-12-03 18:26:25 +0100

    User.java

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/Usuario.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/Usuario.java
@@ -1,39 +1,58 @@
 package pt.ipleiria.estg.dei.ei.dae.academics.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
 public class Usuario {
-    private  int id;
-    public  String username;
-    public  String password;
-    public  String email;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    private String email;
 
     public Usuario() {}
 
-    public Usuario(int id, String username, String password) {
-        this.id = id;
+    public Usuario(String username, String password, String email) {
         this.username = username;
         this.password = password;
-        this.email = email;
-    }
-    public int getId() {
-        return id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setEmail(String email) {
         this.email = email;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
