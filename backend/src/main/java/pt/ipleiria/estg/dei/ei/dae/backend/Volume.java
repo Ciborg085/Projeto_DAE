f7950677d5c1eec779373fa837423086f1518b44
Diagoalv <78082444+Diagoalv@users.noreply.github.com>
2024-12-10 17:22:11 +0100

    Volume.java

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/Volume.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/Volume.java
@@ -36,10 +36,11 @@ public class Volume {
         Delivered
     }
 
-    // Getters y Setters
+    
     public int getId() {
         return id;
     }
+
 
     public void setId(int id) {
         this.id = id;
