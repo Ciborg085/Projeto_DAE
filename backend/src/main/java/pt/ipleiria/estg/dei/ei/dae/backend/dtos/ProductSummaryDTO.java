2a7fbdd16d8cdc0c1d9f42483f30747f6759138b
Bernardo Frija <1903bernardo@gmail.com>
2025-01-13 16:30:40 +0000

    Updated ProductSummaryDTO

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/dtos/ProductSummaryDTO.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/dtos/ProductSummaryDTO.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
 
 public class ProductSummaryDTO implements Serializable {
     private long id;
+    private String name;
+    private String brand;
+    private int quantity;
 
     public ProductSummaryDTO() {
     }
