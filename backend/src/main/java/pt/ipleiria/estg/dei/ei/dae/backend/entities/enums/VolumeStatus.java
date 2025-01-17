05f3b0d4e18ea56579e1f0dc000837d3a9b9fdb5
Bernardo Frija <1903bernardo@gmail.com>
2025-01-17 00:18:41 +0000

    Updated Volume enum

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/enums/VolumeStatus.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/enums/VolumeStatus.java
@@ -1,8 +1,30 @@
 package pt.ipleiria.estg.dei.ei.dae.backend.entities.enums;
 
+import pt.ipleiria.estg.dei.ei.dae.backend.exceptions.IllegalArgumentException;
+
 public enum VolumeStatus {
-    SENT,
-    ABORTED,
-    DELIVERED
+    SENT("sent"),
+    ABORTED("aborted"),
+    DELIVERED("delivered");
+
+    private final String status;
+
+    VolumeStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public static VolumeStatus fromString(String status) throws IllegalArgumentException {
+        for (VolumeStatus volumeStatus :  VolumeStatus.values()) {
+            if (volumeStatus.status.equals(status)) {
+                return volumeStatus;
+            }
+        }
+        throw new IllegalArgumentException("Unknown status: " + status);
+    }
 }
 
